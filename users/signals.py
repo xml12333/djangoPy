26c79a719a75fe2bcebe6b601bef5459622fade9
Nik T <xml12333@gmail.com>
2023-08-29 16:05:20 +0300

    comment signals for db load

--- users/signals.py
+++ users/signals.py
@@ -4,49 +4,45 @@ from django.dispatch import receiver
 from .models import Profile
 from django.core.mail import send_mail
 from django.conf import settings
-import os
-from dotenv import load_dotenv
 # @receiver(post_save, sender=Profile)
-load_dotenv()
 
-def createProfile(sender, instance, created, **kwargs):
-    if created:
-        user = instance
-        profile = Profile.objects.create(
-            user=user,
-            username=user.username,
-            email=user.email,
-            name=user.first_name,
-        )
-        subject = 'Welcome to DevSearch'
-        message = 'We are glad you are here!'
-        send_mail(
-            subject,
-            message,
-            settings.EMAIL_HOST_USER,
-            [profile.email],
-            fail_silently=False,
-        )
-
-
-def updateUser(sender, instance, created, **kwargs):
-    profile = instance
-    user = profile.user
-    if created == False:
-        user.first_name = profile.name
-        user.username = profile.username
-        user.email = profile.email
-        user.save()
+# def createProfile(sender, instance, created, **kwargs):
+#     if created:
+#         user = instance
+#         profile = Profile.objects.create(
+#             user=user,
+#             username=user.username,
+#             email=user.email,
+#             name=user.first_name,
+#         )
+#         subject = 'Welcome to DevSearch'
+#         message = 'We are glad you are here!'
+#         send_mail(
+#             subject,
+#             message,
+#             settings.EMAIL_HOST_USER,
+#             [profile.email],
+#             fail_silently=False,
+#         )
 
 
-def deleteUser(sender, instance, **kwargs):
-    try:
-        user = instance.user
-        user.delete()
-    except:
-        pass
+# def updateUser(sender, instance, created, **kwargs):
+#     profile = instance
+#     user = profile.user
+#     if created == False:
+#         user.first_name = profile.name
+#         user.username = profile.username
+#         user.email = profile.email
+#         user.save()
 
-if os.getenv('ENABLE_SIGNALS'):
-    post_save.connect(updateUser, sender=Profile)
-    post_save.connect(createProfile, sender=User)
-    post_delete.connect(deleteUser, sender=Profile)
+
+# def deleteUser(sender, instance, **kwargs):
+#     try:
+#         user = instance.user
+#         user.delete()
+#     except:
+#         pass
+
+# post_save.connect(updateUser, sender=Profile)
+# post_save.connect(createProfile, sender=User)
+# post_delete.connect(deleteUser, sender=Profile)
