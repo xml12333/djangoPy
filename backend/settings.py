7b5e98184e8c827d0c13b4fd1707f057a09400fc
Nik T <xml12333@gmail.com>
2023-12-09 22:29:29 +0200

    add whitenoise for collection static

--- backend/settings.py
+++ backend/settings.py
@@ -59,6 +59,7 @@ SIMPLE_JWT = {
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
