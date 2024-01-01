83fd43d93fdd0a5d8587ec26fc3136d0cd0aee7f
Nik T <xml12333@gmail.com>
2024-01-01 17:02:18 +0200

    another fix

--- api/core/asgi.py
+++ api/core/asgi.py
@@ -6,9 +6,12 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
-import chat.routing
+
 import os
+import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+django.setup()
+import chat.routing
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django_channels_jwt_auth_middleware.auth import JWTAuthMiddlewareStack
