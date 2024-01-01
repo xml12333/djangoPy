d9b3bc8178f5fa832fd723d14cc312a77b7313db
Nik T <xml12333@gmail.com>
2024-01-01 16:47:13 +0200

    fix settings

--- api/core/asgi.py
+++ api/core/asgi.py
@@ -12,7 +12,8 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django_channels_jwt_auth_middleware.auth import JWTAuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-
+from django.conf import settings
+settings.configure()
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 # application = get_asgi_application()
 
